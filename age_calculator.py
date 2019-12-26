b8b69a423d12ff22dc507a3fb03bb6967e45216b
Manal Alkandari <manal_alkandri@hotmail.com>
2019-12-26 16:19:40 +0300

    finish age

--- age_calculator.py
+++ age_calculator.py
@@ -2,22 +2,24 @@ from datetime import date
 
 
 def check_birthdate(year1,month1,day1):
-	today = date.today() 
+    today = date.today() 
 
-	if today.year > year1.year:
-	   return print('the birthdate is invalid ')
 
-	if today.year < year1.year:
-	   return calculate_age(year1,month1,day1)
+    if today.year < year1:
+        return False
+
+    if today.year > year1:
+       return True
      
 
 
 def calculate_age(year1,month1,day1):
-	today = date.today()
-	#age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-	age = today.year - year1.year - ((today.month, today.day) < (month1.month, day1.day))
-    print (" you are  "  ,  age , "  years old. " )
+    today = date.today()
+    #age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    age = today.year - year1 - ((today.month, today.day) < (month1, day1))
+    print (" you are  ", age, "  years old. " )
 
+    
 
 
 
@@ -27,7 +29,10 @@ year = int(input('Enter year of birth:   '))
 month = int(input('Enter month of birth:   '))
 day = int(input('Enter day of birth:   '))
 
-check_birthdate(year,month,day)
+if check_birthdate(year,month,day) == True:
+    calculate_age(year, month, day)
+else:
+    print("The birthdate is invalid.")
 
 
 
