52c32f6d6c0237dce639d569901ba81635c32223
Manal Alkandari <manal_alkandri@hotmail.com>
2020-01-08 19:13:53 +0300

    done

--- cashier.py
+++ cashier.py
@@ -1,60 +1,48 @@
-apple_price  = 0.2
-carrot_price = 0.1
-flour_price  = 1.3
-water_price  = 0.05
+cashier_item = {}
+cashier_history = []
 
-total = 0.0
+stop = False
 
-print ("\n\n please choose items (apples,carrot,flour,water) \n\n ")
+while not stop:
+    name = input("\nenter the item or type done to exit  \n ")
 
+    if name == 'done':
+        stop = True
+        break 
 
-#done   = "true"
-
-#item = input ( " item(enter 'done' when finished):  " )
-item = ""
-
-while ( item != "done") :
-
-    item = input ( " item(enter 'done' when finished):  " )
+    price = input("price per item:  ")
+    quantity = input("quantity purchased:   ")
     
-    if item == "apples":
-        print ( " price: " , apple_price )
-        quantity_apple = int(input ("Qusntity:  " )) 
-        total_apple = apple_price * quantity_apple
-        total += total_apple
 
-    elif item == "carrot" : 
-        print ( " price: " , carrot_price )
-        quantity_carrot = int(input ("Qusntity:  " )) 	
-        total_carrot = carrot_price * quantity_carrot
-        total += total_carrot
+    cashier_item = {'item_name':name, 
+                    'quantity':int(quantity),
+                    'price':float(price)}
 
-    elif item == "flour":
-        print ( " price: " , flour_price )
-        quantity_flour = int(input ("Qusntity:  " ))	
-        total_flour = flour_price * quantity_flour
-        total += total_flour
+    cashier_history.append(cashier_item)
+    
+    print ("\n---------------\n")
+    
+    
 
-    elif item == "water":
-        print ( " price: " , water_price )
-        quantity_water = int(input ("Qusntity:  " ))	
-        total_water = water_price * quantity_water
-        total += total_water
-
-    elif item == "done":
-        total = total_apple + total_carrot + total_flour + total_water 
-        done = "true"
-print ("\n\nDone")
+   
 
 
-print ("\n\n------------------")
-print ("     receipt      ")
-print ("------------------")
+print ("\n\n---------------\n")
+print("receipt\n")
+print("--------------\n")
 
-print ( quantity_apple , " apples " , total_apple , " KD \n ")
-print ( quantity_carrot , " carrot " , total_carrot , " KD \n ")
-print ( quantity_flour , " flour " , total_flour , " KD \n ")
-print ( quantity_water , " water " , total_water , " KD \n ")
+price_total = 0 
+for item in cashier_history:
+   item_total = item['quantity'] * item['price'] 
+   price_total += item_total
+   print("%d   %s     %.3f KD"  %(item['quantity'], item['item_name'] ,item_total)) 
 
-print ("\n---------------------------------------------------\n")
-print ("total price : " , total , " KD " )
+   item_total = 0
+   
+
+print("\n-------------------\n")   
+print("total price  : %.3f KD"  % price_total) 
+
+
+
+
